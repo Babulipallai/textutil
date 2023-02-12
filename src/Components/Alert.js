b04a982edd9ec05437f1712c3eb467dfa4598d36
Babuli Pallai <babulipalai6@gmail.com>
2023-02-12 17:10:47 +0530

    #

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -5,7 +5,7 @@ export default function Alert(props) {
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
   return (
-    <div style={{height : '50px'}}>
+    <div style={{ height: "50px" }}>
       {props.alert && (
         <div
           className={`alert alert-${props.alert.type} alert-dismissible fade show`}
